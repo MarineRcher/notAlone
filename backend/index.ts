13b300f4d55d7df6fcafb43fee9aaa1f85b75696
David GAILLETON <david.gailleton@posteo.net>
2025-02-06 17:03:56 +0100

    Cleanup socket.io

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,5 @@
 import express, { Express, Request, Response } from "express";
 import { Server, Socket } from "socket.io";
-import { joinWaitList } from "./src/controllers/ChatRoomController";
 
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
@@ -22,16 +21,6 @@ app.listen(port, "0.0.0.0", () => {
 io.on('connection', (socket: Socket) => {
     console.log('Client connected');
 
-    socket.on('sendMessage', (message: string) => {
-        console.log('Message received: ', message);
-        io.emit('Message received', message);
-    });
-
-    socket.on('joinChatGroup', (userID: string) => {
-        const result = joinWaitList(userID);
-        console.log(result);
-        io.emit(result, "");
-    });
 
     socket.on('disconnect', () => {
         console.log('client disconnected');
