927b5fa7fb1ed6f8339d494e73b2285b07e0d76d
David GAILLETON <david.gailleton@posteo.net>
2025-05-30 15:05:39 +0200

    ADD: unit tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,31 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-    testEnvironment: "node",
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: [
+        '**/__tests__/**/*.test.ts',
+        '**/__tests__/**/*.test.js'
+    ],
     transform: {
-        "^.+.tsx?$": ["ts-jest", {}],
+        '^.+\\.ts$': 'ts-jest',
     },
+    collectCoverageFrom: [
+        'src/**/*.{ts,js}',
+        '!src/**/*.d.ts',
+        '!src/__tests__/**',
+        '!src/migrations/**',
+        '!src/config/**'
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: [
+        'text',
+        'lcov',
+        'html'
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+    testTimeout: 10000,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true
 };
